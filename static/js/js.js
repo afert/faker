1e57a23c1cb48243f4c6182ec86cd3611e782de5
BC <danhuaers@foxmail.com>
2018-11-08 16:12:31 +0800

    11.8

--- static/js/js.js
+++ static/js/js.js
@@ -1,14 +1,44 @@
-function checkField(s){
+function checkField()
+{  
 
-	var regu=/[\w\d\s\.\?\!]/;
-	var re=new RegExp(regu);
+ var letterNumber = /^[\d\w\s\.\?\!ñáéíóúü]+$/;  
+ var page1=$('#page1').val()
+ var page2=$('#page2').val()
+    if(page1.match(letterNumber) && page2.match(letterNumber) )   
+      {  
+       return true; 
+      }  
+    else if(page1.match(letterNumber) && !page2.match(letterNumber)){
 
-	if(re.test(s)){
+    	// $('#page2').after(' <div class="alert alert-danger" role="alert"> <strong>Oh snap!</strong> Change a few things up and try submitting again.</div>');
+    		$('#page2tip').hide(10000);
+    	    $("form").submit(function(e){
+    		e.preventDefault(); });
+    	    
+    }
+    else  if(!page1.match(letterNumber) && page2.match(letterNumber)){
+    	//$('#page1').after(' <div class="alert alert-danger" role="alert"> <strong>Oh snap!</strong> Change a few things up and try submitting again.</div>');
 
-		return true;
-	}
-	else{
-
-		$('.textpage').after("ERROR");
-	}
-}
+    	    $("form").submit(function(e){
+    		e.preventDefault(); });
+    	    if($('.alert.alert-danger')){
+    	   		alert('YO');
+    	   		$('.alert.alert-danger').hide(4000);
+    	   		
+    	   }
+    }
+    else if(!page1.match(letterNumber) && !page2.match(letterNumber)){
+    	//$('#page1').after(' <div class="alert alert-danger" role="alert"> <strong>Oh snap!</strong> Change a few things up and try submitting again.</div>');
+    	//$('#page2').after(' <div class="alert alert-danger" role="alert"> <strong>Oh snap!</strong> Change a few things up and try submitting again.</div>');
+    	$("form").submit(function(e){
+    		e.preventDefault(); });
+    }
+    // {   
+    
+    // $(who).after(' <div class="alert alert-danger" role="alert"> <strong>Oh snap!</strong> Change a few things up and try submitting again.</div>');
+    // // $('.alert.alert-danger').hide('5000');
+     
+        
+    // }
+    
+ }
